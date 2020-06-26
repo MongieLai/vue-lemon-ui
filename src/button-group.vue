4d980af59cf7015963a5f68a171036c8a1c895c7
MongieLee <mongielee@163.com>
2020-06-27 00:48:20 +0800

    l-button-group第一个元素不需要margin-left

--- src/button-group.vue
+++ src/button-group.vue
@@ -1,37 +1,41 @@
 <template>
-  <div class='l-button-group'>
+  <div class="l-button-group">
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-    mounted(){
-       for(let node of this.$el.children){
-           let name = node.nodeName.toLowerCase()
-           if(name!=='button'){
-               console.warn(`l-button-group的子元素应使用l-button，而你却使用了${name}`)
-           }
-       }
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== "button") {
+        console.warn(
+          `l-button-group的子元素应使用l-button，而你却使用了${name}`
+        );
+      }
     }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.l-button-group{
-    display: inline-flex;
-    vertical-align:middle;
-    > .l-button{
-        border-radius:0;
-        margin-left: -1px;
-        &:first-child{
-            border-top-left-radius: var(--border-radius);
-            border-bottom-left-radius: var(--border-radius);
-        }
-        &:last-child{
-            border-top-right-radius: var(--border-radius);
-            border-bottom-right-radius: var(--border-radius);
-        }
+.l-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .l-button {
+    border-radius: 0;
+    &:not(first-child) {
+      margin-left: -1px;
     }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+  }
 }
 </style>

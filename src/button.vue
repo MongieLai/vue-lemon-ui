d5e8780a52e8e096fec62fdf9df014a7b81ee9ff
MongieLee <mongielee@163.com>
2020-06-26 23:03:27 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,22 @@
  <template>
-  <button class="l-button">按钮</button>
+  <button class="l-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="iconName" />
+    </svg>
+    <div class="content">
+      <slot />
+    </div>
+  </button>
 </template>
  
  <script>
 export default {
-  template: `
-        
-    `
+  props: ["icon", "iconPosition"],
+  computed: {
+    iconName() {
+      return `#l-${this.icon}`;
+    }
+  }
 };
 </script>
 
@@ -18,6 +28,27 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align:middle;
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.3em;
+    }
+    > .content {
+      order: 1;
+    }
+  }
   &:hover {
     border-color: var(--border-color-hover);
   }

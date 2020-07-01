1fae4047727144a8b3db283ecf419eb08f5fc0e2
MongieLee <mongielee@163.com>
2020-07-01 23:50:10 +0800

    引入mocha

--- src/button.vue
+++ src/button.vue
@@ -9,6 +9,9 @@
 </template>
  
  <script>
+import Vue from "vue";
+import Icon from './icon'
+Vue.component('l-icon',Icon)
 export default {
   props: {
     icon: {},
@@ -19,9 +22,9 @@ export default {
         return value === "left" || value === "right";
       }
     },
-    loading:{
-      type:Boolean,
-      default:false
+    loading: {
+      type: Boolean,
+      default: false
     }
   },
   computed: {

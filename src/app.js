59041e779fdab67e2f3579cb3834697e60f4101b
MongieLee <mongielee@163.com>
2020-07-01 23:06:54 +0800

    使用chai.expect添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import Icon from './icon'
 import ButtonGroup from './button-group'
 Vue.component('l-button', Button)
 Vue.component('l-icon', Icon)
-Vue.component('l-button-group',ButtonGroup)
+Vue.component('l-button-group', ButtonGroup)
 new Vue({
     el: '#app',
     data() {
@@ -12,9 +12,89 @@ new Vue({
             loading1: true
         }
     },
-    methods:{
-        xxx(){
+    methods: {
+        xxx() {
             console.log(111)
         }
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'hahah'
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#l-hahah')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'download',
+            loading: true
+        }
+    })
+    button.$mount()
+    let useElement = button.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#l-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'download',
+            loading: true
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq("1")
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'download',
+            iconPosition:'right'
+        }
+    })
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq("2")
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'download'
+        }
+    })
+    button.$mount()
+    button.$on('click',()=>{console.log(1)})
+    let _button = button.$el
+    _button.click()
+}

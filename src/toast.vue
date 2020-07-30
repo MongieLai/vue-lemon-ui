d355b10de847ff6ae84e6b64f25a66b740e0c90e
MongieLee <mongielee@163.com>
2020-07-30 20:27:25 +0800

    实现autoClose,closeButton,enableHtml,多行文字等功能

--- src/toast.vue
+++ src/toast.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="toast">
-    <slot></slot>
-    <div class="line"></div>
+  <div class="toast" ref="wrapper">
+    <div class="message">
+      <slot v-if="!enableHtml"></slot>
+      <div v-else v-html="$slots.default"></div>
+    </div>
+
+    <div class="line" ref="line"></div>
     <span v-if="closeButton" @click="clickClose()">{{closeButton.message}}</span>
   </div>
 </template>
@@ -26,16 +30,31 @@ export default {
           callBack: undefined
         };
       }
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
-    if (this.autoClose) {
-      setTimeout(() => {
-        this.toastClose();
-      }, this.autoCloseDelay * 1000);
-    }
+    this.execAutoClose();
+    this.updateStyles()
   },
   methods: {
+    execAutoClose() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.toastClose();
+        }, this.autoCloseDelay * 10200);
+      }
+    },
+    updateStyles() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height = `${
+          this.$refs.wrapper.getBoundingClientRect().height
+        }px`;
+      });
+    },
     toastClose() {
       this.$el.remove();
       this.$destroy();
@@ -59,7 +78,7 @@ export default {
   left: 50%;
   transform: translateX(-50%);
   font-size: 14px;
-  height: 40px;
+  min-height: 40px;
   line-height: 1.8;
   display: flex;
   align-items: center;
@@ -67,13 +86,18 @@ export default {
   color: white;
   background-color: rgba(0, 0, 0, 0.75);
   box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 4px;
   > span {
     padding-left: 16px;
+    flex-shrink: 0;
   }
   .line {
     height: 100%;
     border: 1px solid #666;
     margin-left: 16px;
+  }
+  .message {
+    padding: 8px 0;
   }
 }
 </style>

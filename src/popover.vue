1cc3797677df011369bc483335689ef8b6d863c7
MongieLee <mongielee@163.com>
2020-08-01 10:58:55 +0800

    解决当容器有overflow:hiden的bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <slot></slot>
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
+    <div class="content-wrapper" ref="contentWrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
   </div>
@@ -12,27 +14,33 @@ export default {
   name: "LemonPopover",
   data() {
     return {
-      visible: false
+      visible: false,
     };
   },
   methods: {
     xxx() {
       const { log } = console;
       this.visible = !this.visible;
-      log("visible切换了");
       if (this.visible === true) {
-        setTimeout(() => {
-          log("新增一个doucment的监听器");
-          const fn = () => {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper);
+          let {
+            width,
+            height,
+            left,
+            top,
+          } = this.$refs.triggerWrapper.getBoundingClientRect();
+          this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
+          this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
+          let eventHandler = () => {
             this.visible = false;
-            log("删除doucment的监听器");
-            document.removeEventListener("click", fn);
+            document.removeEventListener("click", eventHandler);
           };
-          document.addEventListener("click", fn);
+          document.addEventListener("click", eventHandler);
         });
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -41,12 +49,11 @@ export default {
   display: inline-block;
   vertical-align: top;
   position: relative;
-  .content-wrapper {
-    position: absolute;
-    bottom: 100%;
-    left: 0;
-    border: 1px red solid;
-    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
-  }
+}
+.content-wrapper {
+  position: absolute;
+  border: 1px red solid;
+  box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
 }
 </style>

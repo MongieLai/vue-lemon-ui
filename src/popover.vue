fd36525afdf7975ada0bbf290a79445ae588d21b
MongieLee <mongielee@163.com>
2020-08-01 12:17:21 +0800

    基本功能完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="xxx">
+  <div class="popover" @click="onClick" ref="popover">
     <span ref="triggerWrapper">
       <slot></slot>
     </span>
@@ -18,26 +18,40 @@ export default {
     };
   },
   methods: {
-    xxx() {
-      const { log } = console;
-      this.visible = !this.visible;
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.contentWrapper);
-          let {
-            width,
-            height,
-            left,
-            top,
-          } = this.$refs.triggerWrapper.getBoundingClientRect();
-          this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
-          this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
-          let eventHandler = () => {
-            this.visible = false;
-            document.removeEventListener("click", eventHandler);
-          };
-          document.addEventListener("click", eventHandler);
-        });
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper);
+      let {
+        width,
+        height,
+        left,
+        top,
+      } = this.$refs.triggerWrapper.getBoundingClientRect();
+      this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`;
+      this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`;
+    },
+    onClickDocument(e) {
+      if (this.$refs.popover && !this.$refs.popover.contains(e.target)) {
+        this.close();
+      }
+    },
+    close() {
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    open() {
+      this.visible = true;
+      this.$nextTick(() => {
+        this.positionContent();
+        document.addEventListener("click", this.onClickDocument);
+      });
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     },
   },
